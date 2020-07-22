498652f85d637ec23c26d9b0ebd78be3f38a74ce
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-07-22 18:08:27 +0900

    コメントの整理

--- src/GVSCoder.hs
+++ src/GVSCoder.hs
@@ -18,7 +18,6 @@ import Control.Concurrent (threadDelay)
 import GHC.IO.Handle (hSetEcho)
 import System.IO (stdin)
 
-acPath = "https://atcoder.jp/contests/abc171/submissions/me"
 gvsAgent = "gvscoder/1.0"
 
 -- Loginリクエストの生成
@@ -40,7 +39,7 @@ startGVSCoder = do
     hSetEcho stdin False
     password <- putStrLn "password:" >> BS.getLine
     hSetEcho stdin True
-    contestName <- putStrLn "contest name? (e.g. \"abc171\")" >> BS.getLine
+    contestName <- putStrLn "contest name? (e.g. \"abc001\")" >> BS.getLine
     port <- readFile "portname"
     -- open GVS
     gvs <- openGVS port
@@ -48,26 +47,25 @@ startGVSCoder = do
     -- start curses
     setCurses
     drawLnColored 3 $ (BS.unpack username) ++ "，welcome to GVS-Coder !!"
+
     manager <- newManager tlsManagerSettings
     initReq' <- parseRequest "https://atcoder.jp/login"
     let initReq = initReq' {requestHeaders = [("User-Agent", gvsAgent)]}
     getLoginRes <- httpLbs initReq manager
-    -- BS.putStrLn.toStrict $ responseBody getLoginRes
-    let cookie1 = responseCookieJar getLoginRes
-    -- putStrLn.show $ "GET LoginPage : " ++ show (responseStatus getLoginRes)
+    let cookie = responseCookieJar getLoginRes
+
+    -- extract CSRF token
     let csrfToken = findCSRFToken . toStrict $ responseBody getLoginRes
     case csrfToken of
         Success token -> do
             let user = User{name = username, pass = password, csrf= BS.pack token, contest = contestName}
-            -- BS.putStr "CSRF:Token : " >> BS.putStrLn (csrf user)
             -- Cookieの挿入
-            req <- attatchCookie (makeLoginReq initReq user) cookie1
+            req <- attatchCookie (makeLoginReq initReq user) cookie
             -- loginのPOST
             postLoginRes <- httpLbs req manager
 
             let cookie' = responseCookieJar postLoginRes
             
-            --putStrLn.show $ "POST LoginPage : " ++ show (responseStatus postLoginRes)
             drawLn $ (replicate 10 '=')
             case responseStatus postLoginRes of
                 status200 -> do
@@ -77,7 +75,6 @@ startGVSCoder = do
         Failure e -> drawLn "fail to get CSRF token. try again" >> print e
 
     drawLnColored 1 "GVS-Coder"
-    waitAnyKey
     finishCurse
 
 getACs :: Manager -> User -> IO (User, WholeStatus)
@@ -89,19 +86,19 @@ getACs manager user = do
     }
     req <- attatchCookie req' (cookie user)
     res <- httpLbs req manager
-    let newCookie = responseCookieJar res
+    let newCookie = responseCookieJar res -- getCookie
     let js = judgementP (BS.unpack $ contest user) . BS.unpack . toStrict $ responseBody res
     return (user{cookie = newCookie}, js)
 
 loopGVS :: GVS -> Manager -> User -> WholeStatus -> IO ()
 loopGVS gvs manager user prevStatus = do
     (user', newStatus) <- getACs manager user
+    
     if sumAc newStatus > sumAc prevStatus
-        then drawLnColored 4 "Accepted!" --drawLnColored 3 "Accepted!!"
+        then drawLnColored 4 "Accepted!"
         else if any isWJ newStatus
             then drawLn "Waiting On Judge..."
             else if countExceptAC newStatus > countExceptAC prevStatus
-                --then drawLnColored 6 "Rejected!!" >> drawLnColored 1 "GVS ON" >> gvsON gvs
                 then drawColored 6 "Rejected!!" >> drawLnColored 1 "GVS!!!!" >> gvsON gvs
                 else drawLn "No change..."
     threadDelay (4*10^6) -- micro seconds
@@ -113,4 +110,4 @@ sumAc, countExceptAC :: WholeStatus -> Int
 sumAc = sum.map ((\status -> ac status).snd) 
 
 countExceptAC = sum.map ((\status -> f status).snd) where
-    f status = sum $ map (\g -> g status) [wa, tle, mle, re, ce, qle, ie, wr]
+    f status = sum $ map (\g -> g status) [wa, tle, mle, re, ce, qle]
